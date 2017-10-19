9c42f305a88b5189b8b164b09244d3b9a7b3d4f5
Michael Evans <michael@michaelgregoryevans.com>
2017-10-19 18:03:29 +0000

    added self.all, save, and self.create methods to MTGOEvent class. Defined, event.name, event.date.

--- lib/MTGO_Decklists/MTGOEvent.rb
+++ lib/MTGO_Decklists/MTGOEvent.rb
@@ -3,13 +3,25 @@ class MTGODecklists::MTGOEvent
 
   @@events = []
 
-  def self.recent_events
+  def self.all
+    @@events
+  end
 
-    event_1 = self.new
-    event_1.name = "Competitive Vintage Constructed League"
-    event_1.date = "October 18, 2017"
-    event_1.decks = {}
-    event_1.url = "https://magic.wizards.com/en/articles/archive/mtgo-standings/competitive-vintage-constructed-league-2017-10-18"
+  def save
+    MTGODecklists::MTGOEvent.all << self
+  end
+
+  def self.create
+    self.new.tap{|event| event.save}
+  end
+
+  def self.recent_events
+    event_1 = self.scrape_event
+    #event_1 = self.new
+    #event_1.name = "Competitive Vintage Constructed League"
+    #event_1.date = "October 18, 2017"
+    #event_1.decks = {}
+    #event_1.url = "https://magic.wizards.com/en/articles/archive/mtgo-standings/competitive-vintage-constructed-league-2017-10-18"
 
     event_2 = self.new
     event_2.name = "Competitive Commander Constructed League"
@@ -21,16 +33,19 @@ class MTGODecklists::MTGOEvent
   end
 
   def self.scrape_event
-    event = nil
+    event = self.create
 
     doc = Nokogiri::HTML(open("https://magic.wizards.com/en/content/deck-lists-magic-online-products-game-info"))
 
-    event_1.url = doc.css("div.article-item-extended").first.css("a").attribute("href").value
-    #go to events_url
+    event.name = doc.css("div.article-item-extended").first.css("div.title h3").text
+    event.date = doc.css("div.article-item-extended").first.css("div.title p").text.strip
+    event.url = "https://magic.wizards.com#{doc.css("div.article-item-extended").first.css("a").attribute("href").value}"
     #extract properties
     #instantiate an event
     event
   end
 end
-#event =
-#event_1.name =
+
+#event_1.name = doc.css("div.article-item-extended").first.css("div.title h3").text
+#event_1.name = doc.css("div.article-item-extended").first.css("div.title p").text.strip
+#event_1.url = "https://magic.wizards.com#{doc.css("div.article-item-extended").first.css("a").attribute("href").value}"
