cc984655aa3da095a2fc01554c781517bf858d5d
Michael Evans <michael@michaelgregoryevans.com>
2017-10-19 14:07:04 +0000

    Added 2nd input to cli requesting specific event or all.

--- NOTES.md
+++ NOTES.md
@@ -1,7 +1,15 @@
 Event listing URL:
 http://decks.tcgplayer.com/magic/deck/search?page=1&format=standard&latestset=ixalan&tags[0]=mtgo
 
-App Behavior:
+App High Level Behavior:
   -Posts the top deck results of the 2 most recent MTGO Compettitive Standard League
   -Each Event Posting has a Post Date, and an array of Decklist objects
   -Each Deck has a name, a player, a date, an array of card objects for the main deck, an array of card ojbects for the sideboard
+
+App Flow:
+  - Asks user for number of Standard Tournaments whose results they would like to view.
+  - Lists all tournamnets requested by Date.
+  - Asks user which tournament's decklists they would like to view (individual or all).
+  - Displays decklsits from requested tournaments.
+
+  
--- lib/MTGO_Decklists/cli.rb
+++ lib/MTGO_Decklists/cli.rb
@@ -5,11 +5,11 @@ class MTGODecklists::CLI
 
     puts "Starting from the most recent, how many Competitive Standard Constructed League event results would you like to view?"
 
-    input = gets.chomp.to_i
+    input_1 = gets.chomp
 
-    while input < 1
+    while input_1.to_i < 1
       puts "Please enter a number greater than 0."
-      input = gets.chomp.to_i
+      input_1 = gets.chomp
     end
 
     puts <<-DOC
@@ -18,4 +18,15 @@ class MTGODecklists::CLI
         2. October 16, 2017
     DOC
   end
+
+  puts "Which tournament would you like to select for decklist viewing?  Type in the number from the list above, or type 'all' to display all decklists."
+
+  input_2 = gets.chomp
+
+  while (input_2 != "all" && input_2.to_i < 0) || input_2.to_i > input_1.to_i
+    puts "Please enter a number from the list of events or 'all'."
+    input_2 = gets.chomp.to_i
+  end
+
+  puts "Displaying Decklists"
 end
