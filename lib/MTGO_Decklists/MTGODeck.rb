1bdf62450200c9b12f383b33386de19bcbeec3b4
Michael Evans <michael@michaelgregoryevans.com>
2017-10-20 15:17:55 +0000

    fleshed out MTGODeck.decklists.  It iinitializes Deck object and assigns values for methods: user, card, win_record, sideboard, mainboard.

--- lib/MTGO_Decklists/MTGODeck.rb
+++ lib/MTGO_Decklists/MTGODeck.rb
@@ -1,7 +1,17 @@
 class MTGODecklists::MTGODeck
-  attr_accessor :card_name, :card_name_count, :card_types, :user, :win_record, :sideboard, :mainboard
+  attr_accessor :card_name, :card_name_count, :cards, :user, :win_record, :sideboard, :mainboard
+
+  def initialize
+    @card = {}
 
   def self.decklists(url)
+    deck = self.new
+
+    deck.user = "SPERLING"
+    deck.card = {}
+    deck.win_record = "(5-0)"
+    deck.sideboard = nil
+    deck.mainboard = nil
 
   end
 end
