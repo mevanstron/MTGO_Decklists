065a891793913abbe2f73e57f1c6c032c886d88d
Michael Evans <michael@michaelgregoryevans.com>
2017-10-20 16:10:43 +0000

    corrected type from @card to @cards in MTGODeck.rb. converted commented self.cards to multiline strucure and added sidebaord key.

--- lib/MTGO_Decklists/MTGODeck.rb
+++ lib/MTGO_Decklists/MTGODeck.rb
@@ -2,7 +2,7 @@ class MTGODecklists::MTGODeck
   attr_accessor :cards, :user, :win_record
 
   def initialize
-    @card = {}
+    @cards = {}
 
   def self.decklists(url)
     deck = self.new
@@ -10,10 +10,15 @@ class MTGODecklists::MTGODeck
     deck.user = "SPERLING"
     deck.card = {"creature" => {"Dryad Arbor" => 1,"Dark Confidant" => 3}, "sideboard" => {"Null Rod" => 1, "Swords to Plowshares" => 2}}
     deck.win_record = "(5-0)"
-    deck.sideboard = nil
-    deck.mainboard = nil
 
   end
 end
 
-#self.cards = {"creature" => {"Dryad Arbor" => 1,"Dark Confidant" => 3}}
+#self.cards = {
+  #"creature" => {
+    #"Dryad Arbor" => 1,"Dark Confidant" => 3
+    #}
+    #"sideboard" => {
+      #"Null Rod" => 1, "Swords to Plowshares" => 2
+    #}
+  #}
