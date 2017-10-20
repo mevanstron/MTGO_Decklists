509c8d35ddfef2283af50ec8a9c44e1924e3340a
Michael Evans <michael@michaelgregoryevans.com>
2017-10-20 15:33:04 +0000

    fleshed out card hash in MTGODeck.rb. Removed redundant attr_accessors.

--- lib/MTGO_Decklists/MTGODeck.rb
+++ lib/MTGO_Decklists/MTGODeck.rb
@@ -1,5 +1,5 @@
 class MTGODecklists::MTGODeck
-  attr_accessor :card_name, :card_name_count, :cards, :user, :win_record, :sideboard, :mainboard
+  attr_accessor :cards, :user, :win_record
 
   def initialize
     @card = {}
@@ -8,10 +8,12 @@ class MTGODecklists::MTGODeck
     deck = self.new
 
     deck.user = "SPERLING"
-    deck.card = {}
+    deck.card = {"creature" => {"Dryad Arbor" => 1,"Dark Confidant" => 3}, "sideboard" => {"Null Rod" => 1, "Swords to Plowshares" => 2}}
     deck.win_record = "(5-0)"
     deck.sideboard = nil
     deck.mainboard = nil
 
   end
 end
+
+#self.cards = {"creature" => {"Dryad Arbor" => 1,"Dark Confidant" => 3}}
